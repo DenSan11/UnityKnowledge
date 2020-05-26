8f25994b2d83404cdcab042a23a5b6ba61a39a99
DenSan11 <1612863387@qq.com>
2020-05-26 15:34:53 +0800

    UI框架

---
+++ UIFramework/Assets/Scripts/SUIFW/DemoProject/HeroInfoUIForm.cs
@@ -0,0 +1,45 @@
+﻿/***
+ * 
+ *    Title: "SUIFW" UI框架项目
+ *           主题： 英雄信息显示窗体
+ *    Description: 
+ *           功能： 
+ *                  
+ *    Date: 2017
+ *    Version: 0.1版本
+ *    Modify Recoder: 
+ *    
+ *   
+ */
+using System.Collections;
+using System.Collections.Generic;
+using SUIFW;
+using UnityEngine;
+
+namespace DemoProject
+{
+	public class HeroInfoUIForm : BaseUIForm {
+
+
+		void Awake () 
+        {
+		    //窗体性质
+            CurrentUIType.UIForms_Type = UIFormType.Fixed;  //固定在主窗体上面显示
+
+            //(事件注册)返回开始菜单  (Adding By 2019/09/18)
+            //RigisterButtonObjectEvent("BtnItem1",
+            //    p => OpenUIForm(ProConst.LOGON_FROMS)//LogonUIForm
+            //    );
+
+            RigisterButtonObjectEvent("BtnItem1",
+                p =>
+                {
+                    //CloseUIForm();
+                    OpenUIForm(ProConst.LOGON_FROMS,true);
+                }
+                );
+
+        }
+		
+	}
+}
